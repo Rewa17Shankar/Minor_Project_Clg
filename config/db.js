d9c934945468d468ebb1fc25a48fead0a1485d58
Rewa17Shankar <shankarewajii@gmail.com>
2025-05-03 17:26:58 +0530

    2nd

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
-    user: process.env.DB_USER || "postgres",
-    host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_NAME || "auth_db",
-    password: process.env.DB_PASSWORD || "2210DMBCSE12145",
-    port: process.env.DB_PORT || 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
